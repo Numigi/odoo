62ff21086523eac3051f50359a89bdc8c1547383
David Dufresne <dufresne.david.2@gmail.com>
2018-07-05 13:06:52 -0400

    Remove codacy coverage

--- .docker_files/run_pytest.sh
+++ .docker_files/run_pytest.sh
@@ -4,10 +4,4 @@ TEST_DIR=${1:-/mnt/extra-addons}
 
 cd $LOG_DIR
 
-pytest $TEST_DIR \
-    -v \
-    --cov \
-    --cov-branch \
-    --cov-config /.coveragerc \
-    --cov-report xml \
-    --junit-xml=${LOG_DIR}/junit.xml
+pytest $TEST_DIR -v --junit-xml=${LOG_DIR}/junit.xml
